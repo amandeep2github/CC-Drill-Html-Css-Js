c488ffaa91fc4b990b91f0f556552e790036845c
amasingh15 <amandeep.singh2@publicissapient.com>
2022-11-18 11:02:38 +0530

    splice correct

--- scripts/splice.js
+++ scripts/splice.js
@@ -1,5 +1,6 @@
 var arr = ['a', 'b', 'c'];
 
-console.log(arr.indexOf('b'));// 1
-arr.splice(1,1);
-console.log(arr.indexOf('b'));// 0
+console.log(arr);// 1
+arr.splice(1,1);//removes element from pos 1 to 1
+console.log(arr.length);// 0
+console.log(arr);// 0
